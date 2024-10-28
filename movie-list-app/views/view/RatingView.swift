75df9349e10fd9b9348a0dc45147b1926327c874
Utkarsh Mehta <utmehta@deloitte.com>
2024-10-28 23:55:55 +0530

    Details page made

---
+++ movie-list-app/views/view/RatingView.swift
@@ -0,0 +1,18 @@
+//
+//  RatingView.swift
+//  movie-list-app
+//
+//  Created by Mehta, Utkarsh on 28/10/24.
+//
+
+import SwiftUI
+
+struct RatingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RatingView()
+}
